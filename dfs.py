4f4ba945ac596df68a0d5234313044de2707a3cc
ajinkyagadgil <ajinkyargadgil@gmail.com>
2021-11-10 00:55:41 -0500

    Messy but working

--- dfs.py
+++ dfs.py
@@ -4,29 +4,48 @@ class dfs():
     self.graph = dict()
     self.visited = dict()
     self.stack=list()
-  def add_vertex(self, v):
-    if v in self.graph:
+    
+  def add_vertex(self, v, graph = None, visited = None):
+    if v in graph:
       print("Vertex ", v, " already exists.")
     else:
-      self.graph[v] = []
-      self.visited[v]=0
+      graph[v] = []
+      visited[v]=0
 
   # Add an edge between vertex v1 and v2 with edge weight e
-  def add_edge(self, v1, v2, e):
+  def add_edge(self, v1, v2, e, graph = None):
     # Check if vertex v1 is a valid vertex
-    if v1 not in self.graph:
+    if v1 not in graph:
       print("Vertex ", v1, " does not exist.")
     # Check if vertex v2 is a valid vertex
-    elif v2 not in self.graph:
+    elif v2 not in graph:
       print("Vertex ", v2, " does not exist.")
     else:
       # Since this code is not restricted to a directed or 
       # an undirected graph, an edge between v1 v2 does not
       # imply that an edge exists between v2 and v1
       temp = [v2, e]
-      self.graph[v1].append(temp)
+      graph[v1].append(temp)
       # temp1=[v1,e]             #for undirected
       # self.graph[v2].append(temp1)  #for undirected
+  
+  def reverse_graph(self, original_graph, reverse_graph, rev_graph_visited):
+    for key in original_graph.keys():
+      self.add_vertex(key, reverse_graph, rev_graph_visited)
+    #debug
+    # a = reverse_graph
+    # b = self.graph
+    # c = rev_graph_visited
+    # d = self.visited
+    for src, value in original_graph.items():
+      for dest in value:
+        self.add_edge(dest[0], src, dest[1], reverse_graph)
+      
+    # a = reverse_graph
+    # b = self.graph
+    # c = rev_graph_visited
+    # d = self.visited
+
 
   # Print the graph
   def print_graph(self):
@@ -44,12 +63,37 @@ class dfs():
        
     self.stack.append(v)
 
+  def scc_dfs(self,v, reverse_graph, reverse_visited, res):
+    reverse_visited[v] = 1
+    res.append(v)
+    #print(v, end='')
+    for edges in reverse_graph[v]:
+      if reverse_visited[edges[0]]!=1:
+        self.scc_dfs(edges[0], reverse_graph ,reverse_visited, res)
 
   def dfs_main(self):
     for key, value in self.graph.items():
         if self.visited[key] != 1:
           self.dfs_visit(key,self.visited)
+  
+  def strongly_connected_components_driver(self):
+    reverse_graph = dict()
+    reverse_graph_visited = dict()
+    res = []
+    final = []
+    #DFS on orig graph and creation of stack according to finish times
+    self.dfs_main()
 
+    self.reverse_graph(self.graph, reverse_graph, reverse_graph_visited)
+
+    while self.stack:
+      vertex = self.stack.pop()
+      if reverse_graph_visited[vertex] != 1:
+        self.scc_dfs(vertex, reverse_graph, reverse_graph_visited, res)
+        final.append(res)
+        res = []
+    return final
+      
 # driver code
 d = dfs()
 
@@ -61,14 +105,14 @@ vertices_no = 0
 # d.add_vertex('D')
 # d.add_vertex('W')
 
-d.add_vertex(0)
-d.add_vertex(1)
-d.add_vertex(2)
-d.add_vertex(3)
-d.add_vertex(4)
-d.add_vertex(5)
-d.add_vertex(6)
-d.add_vertex(7)
+d.add_vertex(0,d.graph, d.visited)
+d.add_vertex(1,d.graph, d.visited)
+d.add_vertex(2,d.graph, d.visited)
+d.add_vertex(3,d.graph, d.visited)
+d.add_vertex(4,d.graph, d.visited)
+d.add_vertex(5,d.graph, d.visited)
+d.add_vertex(6,d.graph, d.visited)
+d.add_vertex(7,d.graph, d.visited)
 
 # Add the edges between the vertices by specifying
 # the from and to vertex along with the edge weights.
@@ -76,15 +120,15 @@ d.add_vertex(7)
 # d.add_edge('A', 'C', 2)
 # d.add_edge('D', 'A', 3)
 # d.add_edge('B','D',5)
-d.add_edge(0, 1, 1)
-d.add_edge(1, 2, 5)
-d.add_edge(2, 3, 3)
-d.add_edge(2, 4, 5)
-d.add_edge(3, 0, 1)
-d.add_edge(4, 5, 2)
-d.add_edge(5, 6, 5)
-d.add_edge(6, 4, 5)
-d.add_edge(6, 7, 5)
+d.add_edge(0, 1, 1, d.graph)
+d.add_edge(1, 2, 5, d.graph)
+d.add_edge(2, 3, 3, d.graph)
+d.add_edge(2, 4, 5, d.graph)
+d.add_edge(3, 0, 1, d.graph)
+d.add_edge(4, 5, 2, d.graph)
+d.add_edge(5, 6, 5, d.graph)
+d.add_edge(6, 4, 5, d.graph)
+d.add_edge(6, 7, 5, d.graph)
 
 
 
@@ -93,6 +137,9 @@ d.print_graph()
 # denotes the edge weight.
 print ("Internal representation: ", d.graph)
 print("visited:     ",d.visited)
-d.dfs_main()
-print(d.visited)
-print("stack is",d.stack)
+#d.dfs_main()
+#print(d.visited)
+#print("stack is",d.stack)
+
+#d.reverse_graph(d.graph)
+print(d.strongly_connected_components_driver())
